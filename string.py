ba048e9f0d6f8c3e78779f5770392b961847745e
antipiebse <79756706+antipiebse@users.noreply.github.com>
2021-07-06 23:29:22 +0900

    Add files via upload

---
+++ string.py
@@ -0,0 +1,11 @@
+print('Hello world')
+#escape \
+print('\'hello world\'')
+#new line
+print('h\ne\nl\nl\no')
+#docstring
+print('''h
+e
+l
+l
+o''')
---
+++ string2.py
@@ -0,0 +1,8 @@
+a = 'hello world'
+print(a)
+#length
+print(len(a))
+#index
+print(a[2:3])
+#repeat
+print((a+'\n')*2)
